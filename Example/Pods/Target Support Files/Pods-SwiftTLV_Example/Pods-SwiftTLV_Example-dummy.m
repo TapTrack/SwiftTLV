5ef439e323be19f2fe375de41332726d57512e68
dshalaby <dave@taptrack.com>
2021-06-04 10:10:51 -0400

    Initial commit of skeleton workspace/project

---
+++ Example/Pods/Target Support Files/Pods-SwiftTLV_Example/Pods-SwiftTLV_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftTLV_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftTLV_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SwiftTLV_Tests/Pods-SwiftTLV_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftTLV_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftTLV_Tests
+@end

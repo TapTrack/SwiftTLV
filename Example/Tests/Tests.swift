d07abdbd7f5e27b679114e2d41df370c2bf50307
dshalaby <dave@taptrack.com>
2021-06-04 11:58:24 -0400

    tests running - before updating Quick and Nimble version is Podfile

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,44 +3,69 @@
 import Quick
 import Nimble
 import SwiftTLV
+import Foundation
+
+class SwiftTLVSpec : QuickSpec {
+    override func spec() {
+        describe("A composed TLV") {
+            context("with a length greater than 65279") {
+                it("should be rejected"){
+                    let length : Int = 65279 + 1
+                    let value : [UInt8] = [UInt8](repeating: 0, count: length)
+                    let tag = 1
+                    do{
+                        expect(try TLV(typeVal: tag, value: value)).to(raiseException())
+//                        expect { try TLV(typeVal: tag, value: value)}.to(matchError(TL))
+                    }catch{
+                        
+                    }
+                    
+                }
+            }
+        }
+        
+        
+    }
+}
+
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
         describe("these will fail") {
-
+            
             it("can do maths") {
                 expect(1) == 2
             }
-
+            
             it("can read") {
                 expect("number") == "string"
             }
-
+            
             it("will eventually fail") {
                 expect("time").toEventually( equal("done") )
             }
             
             context("these will pass") {
-
+                
                 it("can do maths") {
                     expect(23) == 23
                 }
-
+                
                 it("can read") {
                     expect("🐮") == "🐮"
                 }
-
+                
                 it("will eventually pass") {
                     var time = "passing"
-
+                    
                     DispatchQueue.main.async {
                         time = "done"
                     }
-
+                    
                     waitUntil { done in
                         Thread.sleep(forTimeInterval: 0.5)
                         expect(time) == "done"
-
+                        
                         done()
                     }
                 }
